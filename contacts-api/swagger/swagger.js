ca32a4dad8cfab77cccb596e5a42ec1308ece426
chiomaezekwe <chummyankie@gmail.com>
2025-10-02 05:40:59 +0100

    modified swagger to use production values for host url and schemes

--- contacts-api/swagger/swagger.js
+++ contacts-api/swagger/swagger.js
@@ -8,9 +8,9 @@ const swaggerDefinition = {
     version: '1.0.0',
     description: 'Swagger 2.0 documentation for Contacts API',
   },
-  host: 'localhost:8080',       // Change to Render URL "three41-project-bzcd.onrender.com" when deploying to Production, and on dev, use "localhost:8080". 
-  basePath: '/',                // base path
-  schemes: ['http', 'https'],   // Use "https" in Production on Render and "http" in dev 
+  host: 'three41-project-bzcd.onrender.com',    // Change to Render URL "three41-project-bzcd.onrender.com" when deploying to Production, and on dev, use "localhost:8080". 
+  basePath: '/',                               // base path
+  schemes: ['https', 'http'],                 // Use "https" in Production on Render and "http" in dev 
 };
 
 const options = {
