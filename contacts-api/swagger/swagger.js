0700ce4027e65929498c6dfcb681378dcd2a9abd
chiomaezekwe <chummyankie@gmail.com>
2025-10-02 05:54:38 +0100

    update to swagger to remove schemes dropdown

--- contacts-api/swagger/swagger.js
+++ contacts-api/swagger/swagger.js
@@ -10,7 +10,7 @@ const swaggerDefinition = {
   },
   host: 'three41-project-bzcd.onrender.com',    // Change to Render URL "three41-project-bzcd.onrender.com" when deploying to Production, and on dev, use "localhost:8080". 
   basePath: '/',                               // base path
-  schemes: ['https', 'http'],                 // Use "https" in Production on Render and "http" in dev 
+  schemes: ['https'],                 // Use "https" in Production on Render and "http" in dev 
 };
 
 const options = {
