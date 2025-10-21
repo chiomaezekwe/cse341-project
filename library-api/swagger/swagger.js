c15710eb2a6ab1eba999965eca46e6fe173d13f6
chiomaezekwe <chummyankie@gmail.com>
2025-10-21 01:29:29 +0100

    modified swagger

--- library-api/swagger/swagger.js
+++ library-api/swagger/swagger.js
@@ -8,9 +8,9 @@ const options = {
       version: '1.0.0',
       description: 'Swagger 2.0 documentation for a REST API used for managing book collection.',
     },
-    host: 'localhost:8080',   // Change to Render URL "cse341-library-w7vq.onrender.com" when deploying to Production, and on dev, use "localhost:8080". 
+    host: 'cse341-library-w7vq.onrender.com',   // Change to Render URL "cse341-library-w7vq.onrender.com" when deploying to Production, and on dev, use "localhost:8080". 
     basePath: '/api',         //  Your base route
-    schemes: ['http'],         // Use "https" in Production on Render and "http" in dev 
+    schemes: ['https'],         // Use "https" in Production on Render and "http" in dev 
 
     securityDefinitions: {
       Bearer: {
